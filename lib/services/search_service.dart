125d7aa4ae7a75be76ac7e4786a7063c26ce22b1
Chinmai <chinmaipottayil2013@gmail.com>
2026-09-17 11:40:57 +0530

    Added search method

--- lib/services/search_service.dart
+++ lib/services/search_service.dart
@@ -3,7 +3,7 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 class SearchService {
   Future<List<dynamic>>searchProducts(String query)async{
-    final String baseUri='https://dummyjson.com/products/search?q=$query';
+    final String baseUri='https://dummyjson.com/products/search?q=${query.toLowerCase()}';
     final response=await http.get(Uri.parse(baseUri));
     if(response.statusCode==200){
       final data=jsonDecode(response.body);
